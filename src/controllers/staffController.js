23c282b21a6a9c7cb651aaabd452d092dbf1a3eb
Hoàng Quốc Trung <hoangquoctrung02@hotmail.com>
2023-02-13 15:50:55 +0700

    add Quan API

---
+++ src/controllers/staffController.js
@@ -0,0 +1,34 @@
+import staffService from "../services/staffService";
+
+const handleGetAllStaffOrder = async (req, res) => {
+    const orders = await staffService.getAllStaffOrder();
+
+    return res.status(200).json({
+        code: 0,
+        message: "OK",
+        order: orders,
+    });
+};
+
+const handleUpdateStatus = async (req, res) => {
+    const id = req.body.id;
+    console.log(`id: ${id}`);
+    if (!id)
+        return res.status(200).json({
+            code: 1,
+            message: "Missing required parameters",
+        });
+
+    const order = await staffService.updateStatusOrder(id)
+    return res.status(200).json({
+        code: 0,
+        message: "Update successfully",
+        order: order
+    })
+};
+
+
+module.exports = {
+    handleGetAllStaffOrder: handleGetAllStaffOrder,
+    handleUpdateStatus: handleUpdateStatus
+};

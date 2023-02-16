822ead2f09cd463d714f33128673281b7b243a31
DucQuan <84904517+QuanBKIT1@users.noreply.github.com>
2023-02-16 23:38:34 +0700

    Quan: add data sample + update staffService

--- src/services/staffService.js
+++ src/services/staffService.js
@@ -3,13 +3,31 @@ import db from "../models";
 const getAllStaffOrder = (status) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const orders = await db.sequelize.query(
-        `SELECT orders.id, users.user_name, items.name, items.image_link, orderdetails.quantity, orders.total_price, orders.shipping_address, vouchers.voucher_code, orders.createdAt, orders.status FROM orders INNER JOIN orderdetails ON orders.id = orderdetails.order_id INNER JOIN users ON orders.user_id = users.id LEFT JOIN vouchers ON orders.voucher_id = vouchers.id INNER JOIN items ON orderdetails.item_id = items.id WHERE orders.status = ${status} ORDER BY orders.createdAt ASC;`,
+      let orders = await db.sequelize.query(
+        //   `SELECT Orders.id, Users.user_name, Orders.total_price, Orders.shipping_address, Vouchers.voucher_code, Orders.createdAt, Orders.status FROM Orders INNER JOIN users ON Orders.user_id = Users.id LEFT JOIN Vouchers ON Orders.voucher_id = Vouchers.id WHERE Orders.status = ${status} ORDER BY Orders.createdAt ${
+        //     status == 0 ? "ASC" : "DESC"
+        //   };`
+        `SELECT orders.id, users.user_name, orders.total_price, orders.shipping_address, vouchers.voucher_code, orders.createdAt, orders.status FROM orders INNER JOIN users ON orders.user_id = users.id LEFT JOIN vouchers ON orders.voucher_id = vouchers.id WHERE orders.status = ${status} ORDER BY orders.createdAt ${
+          status == 0 ? "ASC" : "DESC"
+        };`,
         {
           type: db.sequelize.QueryTypes.SELECT,
         }
       );
-      resolve(orders);
+      const promiseArray = orders.map(async (value) => {
+        const detail_order = await db.sequelize.query(
+          // `SELECT Items.name, Orderdetails.quantity FROM Orderdetails INNER JOIN Items ON Orderdetails.item_id = Items.id WHERE Orderdetails.order_id = ${value.id}`,
+          `SELECT items.name, orderdetails.quantity FROM orderdetails INNER JOIN items ON orderdetails.item_id = items.id WHERE orderdetails.order_id = ${value.id}`,
+          { type: db.sequelize.QueryTypes.SELECT }
+        );
+        const newValue = { ...value, detail_order };
+        return newValue;
+      });
+
+      Promise.all(promiseArray).then((staffOrders) => {
+        console.log(staffOrders);
+        return resolve(staffOrders);
+      });
     } catch (error) {
       reject(error);
     }
